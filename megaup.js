0b8e852c461667d645f777941758b6bab85eaec6
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2025-06-24 22:15:29 +0530

    Create megaup.js

---
+++ megaup.js
@@ -0,0 +1,745 @@
+var a = 255;
+var h = 256;
+let base = function () {};
+base[89136] = (n => ({
+  V6MGRPG: function () {
+    var t;
+    var i = arguments;
+    switch (n) {
+      case 49:
+        t = i[4] - i[0] + i[2] - i[1] + i[3];
+        break;
+      case 36:
+        t = (i[0] + i[3] + i[1]) * i[4] - i[2];
+        break;
+      case 44:
+        t = (i[1] - i[3] - i[2]) * i[4] + i[0];
+        break;
+      case 16:
+        t = i[1] + i[2] - i[0];
+        break;
+      case 42:
+        t = (i[1] - i[3] - i[2]) / i[0] + i[4];
+        break;
+      case 43:
+        t = (i[4] - i[3]) * i[2] / i[0] - i[1];
+        break;
+      case 45:
+        t = i[2] / i[0] * i[3] - i[1] + i[4];
+        break;
+      case 17:
+        t = (i[3] + i[1]) / i[0] - i[2] + i[4];
+        break;
+      case 53:
+        t = (-i[1] / i[4] - i[0]) / i[3] + i[2];
+        break;
+      case 2:
+        t = i[1] * i[0] - i[2];
+        break;
+      case 30:
+        t = i[3] + i[0] - i[4] + i[2] - i[1];
+        break;
+      case 32:
+        t = i[4] / i[1] - i[2] + i[3] + i[0];
+        break;
+      case 38:
+        t = (i[4] - i[2]) * i[1] - i[0] + i[3];
+        break;
+      case 6:
+        t = i[0] * i[2] * i[1] - i[3];
+        break;
+      case 48:
+        t = -i[0] - i[1] + i[2];
+        break;
+      case 20:
+        t = i[2] / i[0] - i[1] - i[4] + i[3];
+        break;
+      case 10:
+        t = -i[3] - i[0] + i[1] + i[2];
+        break;
+      case 12:
+        t = (i[2] + i[3]) * i[4] + i[0] - i[1];
+        break;
+      case 39:
+        t = i[2] / i[3] * i[1] - i[0];
+        break;
+      case 35:
+        t = (i[1] - i[3]) / i[2] + i[0];
+        break;
+      case 0:
+        t = (i[2] - i[0]) * i[1] - i[3];
+        break;
+      case 46:
+        t = -i[3] * i[1] - i[2] + i[0];
+        break;
+      case 37:
+        t = (i[2] - i[1] + i[0]) * i[3] - i[4];
+        break;
+      case 50:
+        t = (i[2] + i[0]) * i[1] - i[3];
+        break;
+      case 8:
+        t = i[0] - i[2] + i[3] + i[1];
+        break;
+      case 13:
+        t = i[0] / i[2] - i[1];
+        break;
+      case 52:
+        t = i[4] / i[1] * i[2] * i[0] - i[3];
+        break;
+      case 19:
+        t = i[0] - i[2] + i[1];
+        break;
+      case 51:
+        t = (-i[0] + i[1]) / i[2] * i[3] - i[4];
+        break;
+      case 5:
+        t = i[4] * i[3] / i[1] + i[2] + i[0];
+        break;
+      case 41:
+        t = i[0] - i[3] - i[1] - i[2] - i[4];
+        break;
+      case 15:
+        t = i[2] - i[0] - i[1];
+        break;
+      case 25:
+        t = i[2] - i[1] - i[0] + i[3];
+        break;
+      case 9:
+        t = -i[1] + i[0];
+        break;
+      case 23:
+        t = (i[2] / i[3] - i[4]) * i[0] + i[1];
+        break;
+      case 24:
+        t = i[0] / i[1] * i[2] + i[3];
+        break;
+      case 26:
+        t = i[1] + i[2] + i[0];
+        break;
+      case 47:
+        t = (i[0] * i[4] - i[1]) * i[3] - i[2];
+        break;
+      case 11:
+        t = -i[3] / i[0] - i[2] + i[1];
+        break;
+      case 27:
+        t = (i[0] + i[1] + i[2]) / i[3] + i[4];
+        break;
+      case 29:
+        t = i[3] * i[2] * i[4] * i[1] - i[0];
+        break;
+      case 33:
+        t = i[3] / i[2] + i[1] + i[4] - i[0];
+        break;
+      case 7:
+        t = -i[0] * i[2] + i[1];
+        break;
+      case 3:
+        t = -i[2] / i[3] * i[1] + i[0];
+        break;
+      case 4:
+        t = i[2] / i[1] + i[0];
+        break;
+      case 22:
+        t = i[3] / i[0] - i[1] + i[2];
+        break;
+      case 34:
+        t = i[0] + i[1] - i[2] - i[3];
+        break;
+      case 40:
+        t = -i[3] / i[1] / i[2] + i[0];
+        break;
+      case 28:
+        t = -i[0] + i[3] + i[2] - i[1];
+        break;
+      case 54:
+        t = i[0] * i[2] - i[3] - i[1];
+        break;
+      case 31:
+        t = (i[2] - i[3]) / i[0] / i[4] + i[1];
+        break;
+      case 14:
+        t = i[2] + i[1] + i[3] - i[0];
+        break;
+      case 1:
+        t = i[0] - i[1];
+        break;
+      case 18:
+        t = (i[1] + i[0]) * i[4] * i[3] - i[2];
+        break;
+      case 21:
+        t = i[1] + i[0];
+        break;
+    }
+    return t;
+  },
+  K$wRsxI: function (t) {
+    n = t;
+  }
+}))();
+var f = base;
+f.o9 = f[89136].K$wRsxI;
+f.p9 = f[89136].V6MGRPG;
+f.I$ = f[89136].V6MGRPG;
+f.O8 = f[89136].K$wRsxI;
+
+// Function 1
+function $(t) {
+  var i;
+  i = [arguments];
+  f.O8(21);
+  var n = f.I$(93, 15);
+  return (i[0][0] + n) % h;
+}
+function C(t) {
+  return [arguments][0][0] ^ 234;
+}
+function st(t) {
+  var i;
+  i = [arguments];
+  f.o9(15);
+  var n = f.p9(10, 85, 100);
+  f.O8(7);
+  var r = f.I$(9, 152, 16);
+  f.o9(37);
+  var e = f.p9(19, 17, 0, 4, 3);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function yt(t) {
+  var i;
+  i = [arguments];
+  f.o9(1);
+  var n = f.I$(1152, 1080);
+  return i[0][0] ^ n;
+}
+function M(t) {
+  var i;
+  i = [arguments];
+  f.o9(16);
+  var n = f.I$(25, 18, 12);
+  f.o9(16);
+  var r = f.I$(40, 40, 8);
+  f.O8(32);
+  var e = f.p9(7, 10, 8, 4, 20);
+  return (i[0][0] >>> n | i[0][0] << r - e) & a;
+}
+function u(t) {
+  var i;
+  i = [arguments];
+  f.o9(0);
+  var n = f.I$(5, 2, 19, 23);
+  f.O8(1);
+  var r = f.p9(32, 24);
+  f.o9(2);
+  var e = f.p9(20, 2, 35);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function g(t) {
+  var i;
+  i = [arguments];
+  f.o9(5);
+  var n = f.I$(70, 5, 18, 17, 15);
+  return (i[0][0] - n + h) % h;
+}
+function w(t) {
+  var i;
+  i = [arguments];
+  f.O8(6);
+  var n = f.p9(22, 15, 11, 3624);
+  f.o9(7);
+  var r = f.I$(8, 64, 7);
+  f.O8(8);
+  var e = f.I$(0, 21, 18, 3);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function o(t) {
+  var i;
+  i = [arguments];
+  f.o9(3);
+  var n = f.I$(12, 9, 1, 1);
+  f.O8(4);
+  var r = f.I$(7, 1, 1);
+  f.o9(1);
+  var e = f.p9(33, 30);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function P(t) {
+  var i;
+  i = [arguments];
+  f.O8(42);
+  var n = f.I$(1, 0, 2, 3, 9);
+  f.O8(16);
+  var r = f.I$(161, 152, 17);
+  f.O8(43);
+  var e = f.I$(2, 12, 16, 10, 12);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function ct(t) {
+  var i;
+  (i = [arguments])[1] = "\0ÀÎùwRW\r\rNÏ\tÐ%L-fmØálãÃN"; // KEY-1:b()
+  i[4] = 0;
+  i[7] = 0;
+  i[6] = [];
+  while (i[0][0].length) {
+    if (i[7]++ < 5) {
+      i[0][0].shift();
+    }
+    i[3] = i[0][0].shift();
+    switch (i[4] % 10) {
+      case 0:
+        i[3] = P(i[3]);
+        break;
+      case 1:
+        i[3] = o(i[3]);
+        break;
+      case 2:
+        i[3] = w(i[3]);
+        break;
+      case 3:
+        i[3] = g(i[3]);
+        break;
+      case 4:
+        i[3] = w(i[3]);
+        break;
+      case 5:
+        i[3] = P(i[3]);
+        break;
+      case 6:
+        i[3] = w(i[3]);
+        break;
+      case 7:
+        i[3] = u(i[3]);
+        break;
+      case 8:
+        i[3] = M(i[3]);
+        break;
+      case 9:
+        i[3] = $(i[3]);
+        break;
+    }
+    f.o9(12);
+    var n = f.I$(6, 239, 57, 2, 4);
+    f.o9(1);
+    var r = f.p9(13, 9);
+    f.o9(38);
+    var e = f.I$(16, 7, 7, 62, 5);
+    i[3] = i[n] ^ i[1].charCodeAt(i[r] % e);
+    i[6].push(i[3] & a);
+    i[4]++;
+  }
+  return i[6];
+}
+function D(a) {
+  return a.split("").map(function (b) {
+    return b.charCodeAt(0);
+  });
+}
+function F(t) {
+  var i;
+  (i = [arguments])[4] = i[0][0];
+  f.o9(31);
+  var n = f.p9(22, 2, 48, 4, 1);
+  f.o9(1);
+  var r = f.I$(76, 72);
+  i[1] = n - i[0][0].length % r;
+  if (i[1] < 4) {
+    i[4] += "=".repeat(i[1]);
+  }
+  i[4] = i[4].replace(/\055/g, "+").replace(/\137/g, "/");
+  return atob(i[4]);
+}
+// Function-2 - Order
+function at() {
+  return "gþüúº\xA0yjH0º{,ç5<?E8é×"; //KEY:f.C9(10)
+}
+function zt(t) {
+  var i;
+  i = [arguments];
+  return D(x(at(), z(i[0][0]))); // KEY-2 GET-KEY:at()
+}
+function R(t) {
+  return zt([arguments][0][0]);
+}
+// Function-3
+function y(t) {
+  var i;
+  i = [arguments];
+  f.O8(14);
+  var n = f.p9(2968, 15, 3128, 9);
+  return i[0][0] ^ n;
+}
+function Z(t) {
+  var i;
+  (i = [arguments])[6] = ";ÌÖ_[q´Ý°ù½MÞö\rºttt½#~y¸gÜ"; // KEY-3:ft()
+  i[7] = 0;
+  i[8] = 0;
+  i[2] = [];
+  while (i[0][0].length) {
+    if (i[8]++ < 6) {
+      i[0][0].shift();
+    }
+    i[9] = i[0][0].shift();
+    switch (i[7] % 10) {
+      case 0:
+        i[9] = y(i[9]);
+        break;
+      case 1:
+        i[9] = M(i[9]);
+        break;
+      case 2:
+        i[9] = C(i[9]);
+        break;
+      case 3:
+        i[9] = st(i[9]);
+        break;
+      case 4:
+        i[9] = yt(i[9]);
+        break;
+      case 5:
+        i[9] = M(i[9]);
+        break;
+      case 6:
+        i[9] = u(i[9]);
+        break;
+      case 7:
+        i[9] = M(i[9]);
+        break;
+      case 8:
+        i[9] = u(i[9]);
+        break;
+      case 9:
+        i[9] = M(i[9]);
+        break;
+    }
+    f.o9(21);
+    var n = f.p9(9, 0);
+    f.O8(22);
+    var r = f.I$(1, 5, 11, 1);
+    f.O8(23);
+    var e = f.p9(4, 36, 4, 1, 5);
+    i[9] = i[n] ^ i[6].charCodeAt(i[r] % e);
+    i[2].push(i[9] & a);
+    i[7]++;
+  }
+  return i[2];
+}
+// Function-4 - Order
+function K() {
+  return "4¶:ÊJLàVùó3ÍºÈÆÔç%2ÛNÞ± "; // KEY:f.N4(21)
+}
+function x(t, i) {
+  var n;
+  (n = [arguments])[4] = [];
+  n[3] = 0;
+  n[6] = 0;
+  n[5] = "";
+  for (n[6] = 0; n[6] < h; n[6]++) {
+    n[4][n[6]] = n[6];
+  }
+  for (n[6] = 0; n[6] < h; n[6]++) {
+    f.O8(1);
+    var r = f.I$(21, 18);
+    f.o9(4);
+    var e = f.p9(3, 1, 1);
+    f.O8(1);
+    var s = f.I$(14, 8);
+    f.O8(1);
+    var a = f.p9(66, 60);
+    n[3] = (n[r] + n[e][n[s]] + n[0][0].charCodeAt(n[a] % n[0][0].length)) % h;
+    n[7] = n[4][n[6]];
+    n[4][n[6]] = n[4][n[3]];
+    n[4][n[3]] = n[7];
+  }
+  n[6] = 0;
+  n[3] = 0;
+  for (n[1] = 0; n[1] < n[0][1].length; n[1]++) {
+    f.O8(28);
+    var c = f.p9(6, 21, 17, 16);
+    f.O8(16);
+    var u = f.p9(27, 12, 16);
+    n[6] = (n[c] + u) % h;
+    f.O8(15);
+    c = f.I$(14, 40, 57);
+    f.o9(2);
+    u = f.p9(8, 8, 60);
+    f.O8(7);
+    var o = f.I$(2, 40, 17);
+    n[3] = (n[c] + n[u][n[o]]) % h;
+    n[7] = n[4][n[6]];
+    n[4][n[6]] = n[4][n[3]];
+    n[4][n[3]] = n[7];
+    n[5] += String.fromCharCode(n[0][1].charCodeAt(n[1]) ^ n[4][(n[4][n[6]] + n[4][n[3]]) % h]);
+  }
+  return n[5];
+}
+function z(t) {
+  var i;
+  i = [arguments];
+  return String.fromCharCode.apply(null, i[0][0]);
+}
+function O(t) {
+  var i;
+  i = [arguments];
+  return D(x(K(), z(i[0][0])));
+}
+function mt(t) {
+  return O([arguments][0][0]);
+}
+// Function-5
+function q(t) {
+  return [arguments][0][0] ^ 194;
+}
+function V(t) {
+  var i;
+  i = [arguments];
+  f.O8(18);
+  var n = f.p9(10, 1, 2502, 19, 12);
+  f.O8(19);
+  var r = f.p9(1, 22, 15);
+  f.o9(20);
+  var e = f.I$(1, 20, 23, 16, 13);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function j(t) {
+  var i;
+  i = [arguments];
+  f.O8(33);
+  var n = f.I$(29, 15, 19, 95, 14);
+  f.O8(34);
+  var r = f.I$(1, 18, 9, 2);
+  f.o9(4);
+  var e = f.I$(4, 9, 9);
+  return (i[0][0] << n | i[0][0] >>> r - e) & a;
+}
+function Bt(t) {
+  var i;
+  (i = [arguments])[9] = "r¨yñ³­U²\rìIõ¸#G3«ÒQ,é­"; // KEY-3 ut()
+  i[4] = 0;
+  i[1] = 0;
+  i[7] = [];
+  while (i[0][0].length) {
+    if (i[1]++ < 8) {
+      i[0][0].shift();
+    }
+    i[3] = i[0][0].shift();
+    switch (i[4] % 10) {
+      case 0:
+        i[3] = g(i[3]);
+        break;
+      case 1:
+        i[3] = u(i[3]);
+        break;
+      case 2:
+        i[3] = o(i[3]);
+        break;
+      case 3:
+        i[3] = j(i[3]);
+        break;
+      case 4:
+        i[3] = o(i[3]);
+        break;
+      case 5:
+        i[3] = C(i[3]);
+        break;
+      case 6:
+        i[3] = V(i[3]);
+        break;
+      case 7:
+        i[3] = q(i[3]);
+        break;
+      case 8:
+        i[3] = o(i[3]);
+        break;
+      case 9:
+        i[3] = M(i[3]);
+        break;
+    }
+    f.O8(17);
+    var n = f.I$(37, 19, 16, 18, 18);
+    f.o9(21);
+    var r = f.p9(3, 1);
+    f.o9(52);
+    var e = f.I$(6, 1, 11, 1024, 16);
+    i[3] = i[n] ^ i[9].charCodeAt(i[r] % e);
+    // s:push // d:charCodeAt
+    i[7].push(i[3] & a);
+    i[4]++;
+  }
+  return i[7];
+}
+// Function-6 - Order
+function gt() {
+  // Z5xWU:atob
+  return "%ð¤_CÓì«¢yq5#-bvûM$gTÒÄ)ØZ²oÈ¾"; // KEY:f.N4(28)
+}
+function Ct(t) {
+  var i;
+  i = [arguments];
+  return D(x(gt(), z(i[0][0])));
+}
+function W(t) {
+  var i;
+  i = [arguments];
+  return Ct(i[0][0]);
+}
+// Function-6
+function J(t) {
+  var i;
+  i = [arguments];
+  i[5] = "XÉ/KõÚ,Bøóá?cóP3ø»TóN Ï"; // KEY (bt()) // GET KEY
+  i[9] = 0;
+  i[8] = 0;
+  i[6] = [];
+  while (i[0][0].length) {
+    if (i[8]++ < 5) {
+      i[0][0].shift();
+    }
+    i[1] = i[0][0].shift();
+    switch (i[9] % 10) {
+      case 0:
+        i[1] = o(i[1]);
+        break;
+      case 1:
+        i[1] = y(i[1]);
+        break;
+      case 2:
+        i[1] = j(i[1]);
+        break;
+      case 3:
+        i[1] = g(i[1]);
+        break;
+      case 4:
+        i[1] = q(i[1]);
+        break;
+      case 5:
+        i[1] = y(i[1]);
+        break;
+      case 6:
+        i[1] = $(i[1]);
+        break;
+      case 7:
+        i[1] = g(i[1]);
+        break;
+      case 8:
+        i[1] = j(i[1]);
+        break;
+      case 9:
+        i[1] = o(i[1]);
+        break;
+    }
+    f.o9(25);
+    var n = f.p9(15, 4, 0, 20);
+    f.O8(26);
+    var r = f.I$(2, 1, 6);
+    f.o9(27);
+    var e = f.p9(448, 9, 3, 460, 31);
+    i[1] = i[n] ^ i[5].charCodeAt(i[r] % e);
+    i[6].push(i[1] & a);
+    i[9]++;
+  }
+  return i[6];
+}
+// Function-7
+function rt() {
+  return "\r t¿¼Ö¥Ö~B\bk8!ÁÜèÑ-A÷©¦ô"; // GET KEY:f.N4(1)
+}
+function ht(t) {
+  var i;
+  i = [arguments];
+  return D(x(rt(), z(i[0][0])));
+}
+function wt(t) {
+  var i;
+  i = [arguments];
+  return ht(i[0][0]);
+}
+// Function-8
+function dt(t) {
+  var i;
+  (i = [arguments])[9] = "÷Ý\b¥ÿÅÿßAÃl_¶\b0n\"M+ütD å)Ù÷"; // GET KEY:it()
+  i[3] = 0;
+  i[1] = 0;
+  i[7] = [];
+  while (i[0][0].length) {
+    if (i[1]++ < 5) {
+      i[0][0].shift();
+    }
+    i[8] = i[0][0].shift();
+    switch (i[3] % 10) {
+      case 0:
+        i[8] = w(i[8]);
+        break;
+      case 1:
+        i[8] = P(i[8]);
+        break;
+      case 2:
+        i[8] = st(i[8]);
+        break;
+      case 3:
+        i[8] = q(i[8]);
+        break;
+      case 4:
+        i[8] = j(i[8]);
+        break;
+      case 5:
+        i[8] = o(i[8]);
+        break;
+      case 6:
+        i[8] = C(i[8]);
+        break;
+      case 7:
+        i[8] = u(i[8]);
+        break;
+      case 8:
+        i[8] = j(i[8]);
+        break;
+      case 9:
+        i[8] = y(i[8]);
+        break;
+    }
+    f.O8(44);
+    var n = f.p9(188, 1, 10, 9, 10);
+    f.o9(1);
+    var r = f.I$(20, 17);
+    f.o9(16);
+    var e = f.p9(3, 17, 18);
+    i[8] = i[n] ^ i[9].charCodeAt(i[r] % e);
+    i[7].push(i[8] & a);
+    i[3]++;
+  }
+  return i[7];
+}
+// Function-9
+function T() {
+  return "Zøs¢\fv¡[·NUbc{D÷!ß%­¨\"<r[ã"; // GET-KEY:f.C9(20)
+}
+function tt(t) {
+  var i;
+  i = [arguments];
+  return D(x(T(), z(i[0][0])));
+}
+function N(t) {
+  return tt([arguments][0][0]);
+}
+function decrypt(t) {
+  var i;
+  (i = [arguments])[0][0] = F(i[0][0]);
+  i[4] = D(i[0][0]);
+
+  // 
+  i[4] = ct(i[4]);
+  i[4] = R(i[4]);
+  i[4] = Z(i[4]);
+  i[4] = mt(i[4]);
+  i[4] = Bt(i[4]);
+  i[4] = W(i[4]);
+  i[4] = J(i[4]);
+  i[4] = wt(i[4]);
+  i[4] = dt(i[4]);
+  i[4] = N(i[4]);
+  i[0][0] = z(i[4]);
+
+  return decodeURIComponent(i[0][0]);
+}
+decrypt("...");
